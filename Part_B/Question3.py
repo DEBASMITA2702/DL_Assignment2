89eede40f60f693c7ef007b890b189a377816ffe
DEBASMITA2702 <69621397+DEBASMITA2702@users.noreply.github.com>
2025-04-17 17:12:46 +0530

    Add files via upload

--- Part_B/Question3.py
+++ Part_B/Question3.py
@@ -8,20 +8,21 @@ def main():
         defining parameteres and loading the dataset
         The values of the parameters are set according to the best model that I had achieved
     ''' 
-    batch_size=8
-    dataLoader=Create_DataLoader.DatasetLoader_create(root='../inaturalist_12K',batch_size=batch_size)
+    batch_size=16
+    dataLoader=Create_DataLoader.DatasetLoader_create(root='/kaggle/input/inaturalist-12k/nature_12K_dataset/inaturalist_12K',batch_size=batch_size)
     trainLoader,valLoader,testLoader=dataLoader.data_loaders()
     
     freezed_layers=5
-    learning_rate=1e-3
+    learning_rate=1e-4
     numOfOutputClasses=10
-    epochs=10
+    epochs=20
 
     '''creating object of the FineTuningModel class'''
     preTrainedModel=ModelPreTrained_test.FineTuningModel(numOfOutputClasses,freezed_layers,learning_rate)
     
     '''creating trainer object by pytorch lightning'''
-    trainer=pl.Trainer(max_epochs=epochs)
+    #trainer=pl.Trainer(max_epochs=epochs)
+    trainer = pl.Trainer(max_epochs=epochs,strategy="ddp_find_unused_parameters_true",devices=1, enable_progress_bar=True)
 
     '''exectuing training and validartion step'''
     trainer.fit(preTrainedModel,trainLoader,valLoader)
